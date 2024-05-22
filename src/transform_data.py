0de9555bac6829e0ffda0beeb1f9dd51a9b07858
JhonGM <45050231+JhonGM@users.noreply.github.com>
2024-05-22 13:19:23 -0500

    File validation
    This PR have a improve to if the specified file exists

--- src/transform_data.py
+++ src/transform_data.py
@@ -2,10 +2,14 @@ import csv
 import pandas as pd
 import numpy as np
 
+
 def read_files(filename):
 
-    df = pd.read_csv(f"files\{filename}.csv")
-    df = df.replace(np.nan, None)
-    json = df.to_dict(orient="records")
+    try:
+        df = pd.read_csv(f"files\{filename}.csv")
+        df = df.replace(np.nan, None)
+        data = df.to_dict(orient="records")
 
-    return json
+        return data
+    except:
+        return "The specified file was not found"
